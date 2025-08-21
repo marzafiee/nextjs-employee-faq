9b49596e545abe3aef4e863be0f3469c03cfd4af
Inez Agbenu <eyra.agbenu@turntabl.io>
2025-08-21 12:02:27 +0000

    modified package due to errors and updated layout and page.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { JetBrains_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const font=Inter({
+  weight: ['400', '500', '600', '700', '800'],
+  subsets: ["latin"]
+})
+
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
+
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+export const metadata: Metadata = {
+  title: "Employee Attendance FAQ Page",
+  description: "Generated using Next.js 15, Tailwind CSS and TypeScript",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${font.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
