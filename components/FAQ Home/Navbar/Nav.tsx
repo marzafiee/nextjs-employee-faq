83c25700cf947c27f824ce9615ab6d97466cfac1
Inez Agbenu <eyra.agbenu@turntabl.io>
2025-08-21 14:54:20 +0000

    added a burger menu icon to the nav bar

--- components/FAQ Home/Navbar/Nav.tsx
+++ components/FAQ Home/Navbar/Nav.tsx
@@ -1,19 +1,24 @@
 import React from 'react'
+import { HiMenuAlt4 } from 'react-icons/hi';
 
 const Nav = () => {
   return (
     <div className="transition-all duration-200 h-[12vh] z-[100] fixed w-full"> {/*nav bar div*/}
-        <div className="flex items-center justify-start w-full mx-auto bg-[#D0FF57] shadow-md"> {/*flexbox for navbar */}
+        <div className="flex w-full mx-auto bg-[#D0FF57] shadow-md sm:px-6 md:px-8"> {/*flexbox for navbar */}
             {/* defining a div for the flex containers for various elements in the navbar but since I only have the logo here, I only created one container to the far left*/}
             
             {/* LOGO */}
-            <div className="flex items-center justify-start px-4 py-2">
-                <img
-                    src="/images/turntabl_Logo.png"
-                    alt="Turntabl Logo"
-                    className="h-6 w-auto max-h-[48px]"
-                />
-            </div>
+          <div className="flex items-center w-full justify-start px-4 py-2">
+            <img
+              src="/images/turntabl_Logo.png"
+              alt="Turntabl Logo"
+              className="h-6 sm:h-8 md:h-10 lg:h-12 w-auto max-h-12"
+            />
+          </div>
+          <div className="flex justify-end pr-6">
+            {/* burger menu*/}
+            <HiMenuAlt4 className="w-5 h-10 auto cursor-pointer text-black lg:hidden" />
+          </div>
         </div>
     </div>
   );
