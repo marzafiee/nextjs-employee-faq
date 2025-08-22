072a67151b8d486ae16696b2e966e4b977907b91
Inez Agbenu <eyra.agbenu@turntabl.io>
2025-08-22 12:44:26 +0000

    made FilterSlider show in the FAQ page

--- components/FAQ Home/FAQ.tsx
+++ components/FAQ Home/FAQ.tsx
@@ -1,11 +1,15 @@
 // here, we have all the FAQ page components like the Hero section, the grid section, etc
 import React from 'react';
 import Hero from "./Hero/Hero";
+import FilterSlider from './Filter Slider/FilterSlider';
 
 const FAQ = () => {
   return (
     <div className="overflow-hidden">
       <Hero />
+      <div className='px-4 py-6'>
+        <FilterSlider />
+      </div>
     </div>
   );
 };
