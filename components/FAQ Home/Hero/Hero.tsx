6c75ceaa2f00c94b3e78eb9f9c66e7430fab449e
Inez Agbenu <eyra.agbenu@turntabl.io>
2025-08-22 13:20:13 +0000

    fixed the spacing on the page by adjusting height of section

--- components/FAQ Home/Hero/Hero.tsx
+++ components/FAQ Home/Hero/Hero.tsx
@@ -1,9 +1,10 @@
+"use client"
 import React from 'react';
 
 const Hero = () => {
   return (
     <div
-      className="relative w-full h-[110vh] sm:h-screen bg-[#1A1A1A] flex flex-col justify-center items-center text-center px-4"
+      className="relative w-full h-[60vh] sm:h-screen bg-[#1A1A1A] flex flex-col justify-center items-center text-center px-4"
       style={{
         backgroundImage: `linear-gradient(
           to bottom, 
@@ -11,12 +12,14 @@ const Hero = () => {
           rgba(122, 150, 3, 0.8) 30%, 
           rgba(26, 26, 26, 1) 85%
         )`,
+        marginBottom: '0px',
+        marginTop: '40px'
       }}
     >
       <h1 className="text-white text-4xl sm:text-5xl font-extrabold mb-6 max-w-4xl">
         &lt;/Frequently Asked Questions (FAQ)_ 
       </h1>
-      <p className="text-gray-300 max-w-3xl leading-relaxed text-base sm:text-lg px-10">
+      <p className="text-gray-300 max-w-3xl leading-relaxed text-base sm:text-lg px-0">
         These are the most commonly asked questions about employee attendance.<br />
         Can’t find what you’re looking for?{' '}
         <a href="#" className="underline text-white hover:text-[#BDFF00]">
