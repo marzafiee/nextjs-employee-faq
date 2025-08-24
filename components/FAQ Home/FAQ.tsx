7adc3e7124e777cb3499c2019948bbfb5a453aa5
Inez Agbenu <eyra.agbenu@turntabl.io>
2025-08-24 23:10:56 +0000

    removed Filter Slider component from FAQ page as it holds the filter logic only

--- components/FAQ Home/FAQ.tsx
+++ components/FAQ Home/FAQ.tsx
@@ -1,7 +1,7 @@
 // here, we have all the FAQ page components like the Hero section, the grid section, etc
 import React from 'react';
 import Hero from "./Hero/Hero";
-import FilterSlider from './Filter Slider/FilterSlider';
+// import FilterSlider from './filter-slider/FilterSlider';
 import FAQCard from './Grid-Contents/FAQCard'
 
 const FAQ = () => {
@@ -11,9 +11,6 @@ const FAQ = () => {
         <Hero />
       </div>
       <div className='px-0' style={{marginTop: '0px'}}>
-        <FilterSlider />
-      </div>
-      <div>
         <FAQCard />
       </div>
     </div>
